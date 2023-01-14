55c9c5387675962e673af9e6403203de5d0bbaf2
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2023-01-14 17:52:28 -0300

    Admin PopupEnginner

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { Dashboard } from '../pages';
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/pagina-inicial' element={<Dashboard/>}/>
+        <Route path='*' element={<Navigate to='/pagina-inicial'/> } />
+      </Routes>
+    </BrowserRouter>
+  );
+};
