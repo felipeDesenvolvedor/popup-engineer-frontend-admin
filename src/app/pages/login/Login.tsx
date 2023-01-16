2299e90c9cf1b504f778e4de8a797881cc4594be
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2023-01-16 09:18:26 -0300

    Teste useMemo

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,6 +1,6 @@
 // import { useNavigate  } from 'react-router-dom';
 
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 // export const Login = () => {
 //   const navigate = useNavigate();
@@ -19,14 +19,24 @@ import { useState } from 'react';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
     
   const handleEntrar = () => {
     console.log('email', email);
   };
 
+  // const emailLength = email.length;
+  const emailLength = useMemo(() => {
+    return email.length + 1;
+  }, [email.length]);
+
+  console.log(emailLength + 1);
+
+
   return (
     <>
       <form>
+        <p>Quantidade de caracteres no email {emailLength}</p>
         <label>
           <span>Email</span>
           <input value={email} type='text' onChange={e => setEmail(e.target.value)}/>
@@ -34,7 +44,7 @@ export const Login = () => {
 
         <label>
           <span>Senha</span>
-          <input type='password'/>
+          <input value={senha} type='password' onChange={e => setSenha(e.target.value)}/>
         </label>
 
         <button onClick={handleEntrar} type='button'>
