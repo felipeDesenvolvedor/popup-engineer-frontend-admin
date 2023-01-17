66ae926eeab637788cd76bb62a3c4749f71a324e
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2023-01-17 09:24:41 -0300

    useRef com objetos

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,17 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
+
+  const counterRef = useRef({counter:0});
+
   return (
     <>
       <p>Dashboard</p>
+      <p>Contador: {counterRef.current.counter}</p>
+
+      <button onClick={() => counterRef.current.counter++}>Somar</button>
+      <button onClick={() => console.log(counterRef.current.counter)}>Logar</button>
       <Link to='/entrar'>Entrar</Link>
     </>
   );
