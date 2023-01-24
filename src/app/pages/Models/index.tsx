6dd87fd4d81c2d3310317a3db169b507b3353ba3
felipe.santos <felipedasilvasantos30@gmail.com>
2023-01-24 17:32:59 -0300

    Navigate for detail model

--- src/app/pages/Models/index.tsx
+++ src/app/pages/Models/index.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import { styled } from '@mui/material/styles';
-import { Grid } from '@mui/material';
+import { Grid, Link } from '@mui/material';
 import { Box } from '@mui/system';
 import { Dashboard } from '../dashboard/Dashboard';
 import Paper from '@mui/material/Paper';
 import { ModelItem } from '../../shared/components/Model/ModelItem';
 import {config} from '../../config';
+import { useNavigate } from 'react-router';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -23,12 +24,26 @@ interface IModel{
 
 export const Models = () => {
   const [lista, setLista] = useState<IModel[]>(config);
+  const navigate = useNavigate();
+
+  const handleRedirectModel = (idModel:any) => {
+
+    console.log('Teste');
+    navigate(`/modelo/${idModel}`);
+  };
 
   const buildModels = () => {
     return lista.map(listModel => {
       return (
         <React.Fragment key={listModel.popupId}>
-          <Grid item xs={4}><Item><ModelItem>{listModel.popupName}</ModelItem></Item></Grid>
+          <Grid item xs={4}>
+            <Item onClick={() => handleRedirectModel(listModel.popupId)}>
+              <ModelItem>
+                {'color="inherit"'}
+                {listModel.popupName}
+              </ModelItem>
+            </Item>
+          </Grid>
         </React.Fragment>
       );
     });
